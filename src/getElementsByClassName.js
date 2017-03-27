6e68cd96ba8fd8d95ac3530ec60f5443413e77da
Hack Reactor Student <student@hackreactor.com>
2017-03-27 12:19:36 -0700

    Complete getElementsByClassName
    Completed getElementsByClassName using recursive calls. Originally utilized
    for loops but refactored to utilize underscore.js function calls.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,23 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  // document.body
+  // classList
+  // childnodes
+  var result = [];
+  
+  var searchDOM = function(node) {
+  	// search doc.body classList for className
+    if ( _.contains(node.classList, className) ) {
+    	result.push(node);
+    }
+    // iterate through doc.body childNodes recursively
+    _.each( node.childNodes, function( j ) {
+  		searchDOM( j );
+  	}); 
+  };
+
+  searchDOM(document.body);
+  return result
+
 };
